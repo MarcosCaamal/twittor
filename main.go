981afab6440a46ba84bf1e44ee7ac0f3b9383daa
MarcosCaamal <m04caamal@gmail.com>
2020-05-01 15:05:48 -0500

    Regsitro de usuario

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/MarcosCaamal/twittor/bd"
+	"github.com/MarcosCaamal/twittor/handlers"
+)
+
+func main() {
+	if bd.ChequeoConnection() == 0 {
+		log.Fatal("Sin conexión a la BD")
+		return
+	}
+
+	handlers.Manejadores()
 }
